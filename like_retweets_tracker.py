2d0607e6fb9db3daa616ff4fee7a72d534d9c65a
mheez <68663997+hendrymason@users.noreply.github.com>
2022-02-23 17:42:41 -0600

    Scaling compatible with periodic updates

--- like_retweets_tracker.py
+++ like_retweets_tracker.py
@@ -2,12 +2,11 @@ from ast import operator
 from re import T
 from typing import Iterable
 from xmlrpc.client import Boolean
+from numpy import true_divide
 import tweepy
 from datetime import *
 import csv
 import time
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
 
 # PURPOSE: a bot intended to compile a twitter users activity toward a targetted twitter & reward the user based on said interaction level
 
@@ -15,18 +14,13 @@ from oauth2client.service_account import ServiceAccountCredentials
 #   {name : [1 $QUAI likes+retweets, 1 $QUAI reply to @quainetwork (with max 2 per day), 5 $QUAI individual tweet about @quainetwork, 
 #   retweets, likes, and replies to your individual tweet about $QUAI -> 5 $QUAI per 10 engagements]}
 
-# Gsheets
-#gc = gspread.service_account(filename='service-key.json')
-#sm_sheet = gc.open_by_key('1pPKG2PwrCr1dlZIvGZB8TQZ1_f0Pey-pgEZ1lvDbsbw')
-#output_sheet = sm_sheet.add_worksheet(title="Rewards Bot Tracked Data", rows=2000, cols=10) 
-# columns: 1. discord name, 2. twitter name, 3. youtube name, 4. like+retweet points, 5. reply points, 6. mention points, 
-#           7. YT subscriber points, 8. YT comment points, 9. multiplier, 10.aggregate total
-
 # CONST
 quai_id = 1306071657174441985
 quai_username = 'quainetwork'
 first_bearer = "AAAAAAAAAAAAAAAAAAAAAEHkXwEAAAAAFgCxzDEOf484cKicUHiV3DO6qcU%3DodAKdbVDHoucR6dlOzPbB719XrDMisbZAWLufgqORgLCLgKUtO"
 second_bearer = "AAAAAAAAAAAAAAAAAAAAANBOYAEAAAAAlLMei9GmJpSazEtiXx6IWZXEbhs%3DYfsE6mbEa2mLrtZKJhjvjzr4gWZ466w1doYhiWGHlwbDyJzTwx"
+third_bearer = "AAAAAAAAAAAAAAAAAAAAAJlpZQEAAAAAjwv7Gg01zTG7ck30cyqxiwQcm9U%3DPNHl3UjpTpKdAjtVcrOs8tZqprtwO6TUKl61XWrXzKKVwgkMDA"
+fourth_bearer = "AAAAAAAAAAAAAAAAAAAAAJxpZQEAAAAAexanqSH%2B10AH6FDOHTOqAlqiI8g%3D9hHfSUY0qNXjDGBwlBRNqhXelDcDYpNvnW4VGtpiroOAlabxaR"
 
 
 # dicts to track and store everything -> {username: [likes+retweets, replies, mentions, engagement rewards], ...}
@@ -34,14 +28,15 @@ sm_rewards = {}
 username_userID = {}
 db_tweets_tracked = []
 all_tweets_tracked = []
-
+last_sm_rewards = {}
 
 # class to track global var in functions
 class tweets_monitor:
+    last_tweet_id = 1459205924573396997 #first tweet id as default
+    twtID_for_reply_mentions = 0
+    completed_check = False
     new_tweet_count = 0
     db_tweet_count = 0
-    last_tweet_id = 1459205924573396997 #first tweet id as default
-    completed_check = False
     db_last_tweet_id = 0
     
     def addedNewTweet(self):
@@ -78,35 +73,40 @@ def sort_List(list_to_sort):
     return list_to_sort
 
 
+
 def read_tweet_data(_monitor):
-    print("read_tweet_data function - reading user reward data")
-    with open("like_retweet_rewards.csv") as lrr:
-        empty_result = is_empty("like_retweet_rewards.csv")
-        if empty_result == "not empty":
-            for line in lrr:
-                line_array = line.strip().split(',')
-                if len(line_array) == 3:
-                    username_userID[line_array[1]] = line_array[0]
-                    sm_rewards[line_array[1]] = line_array[2]
-    print("read_tweet_data function - reading tweet data")
-    with open('client_tweet_data.csv') as ctd:
-        empty_result = is_empty('client_tweet_data.csv')
+    with open('tweet_monitor_data.csv') as ctd:
+        empty_result = is_empty('tweet_monitor_data.csv')
         if empty_result == "not empty":
             for line in ctd:
                 line_array = line.strip().split(': ')
                 if line_array[0] == "Last Check Complete":
                     _monitor.completed_check = bool(line_array[1])
                 if line_array[0] == "Last tweet ID" and _monitor.completed_check == True:
-                    _monitor.last_tweet_id = line_array[1]
                     _monitor.db_last_tweet_id = line_array[1]
+                    _monitor.twtID_for_reply_mentions = line_array[1]
+                    _monitor.last_tweet_id = line_array[1]
         print("read_tweet_data function - closed client data file. set last_tweet_id to " + str(_monitor.last_tweet_id))
+    if _monitor.completed_check == False:
+        print("read_tweet_data function - last check was not completed, reading user reward data")
+        with open("like_retweet_rewards.csv") as lrr:
+            empty_result = is_empty("like_retweet_rewards.csv")
+            if empty_result == "not empty":
+                line_index = 0
+                for line in lrr:
+                    line_array = line.strip().split(',')
+                    if line_index == 0:
+                        line_index += 1
+                        continue
+                    elif len(line_array) == 3:
+                        username_userID[line_array[1]] = line_array[0]
+                        sm_rewards[line_array[1]] = line_array[2]
+    print("read_tweet_data function - reading tweet data")
     # open tweet input csv and create a dictionary from the file data
     with open("tweetsDB.csv") as twts:
         print("read_tweet_data function - opened input data file")
         empty_result = is_empty("tweetsDB.csv")
-        line_count = 0
         for line in twts:
-            line_count += 1
             if empty_result == "not empty":
                 twt_array = line.strip().split(': ')
                 if twt_array[0] == "Valid Tweet Tracked":
@@ -117,18 +117,18 @@ def read_tweet_data(_monitor):
             _monitor.last_tweet_id = db_tweets_tracked[-1]
             print("read_tweet_data function - set last_tweet_id to " + str(_monitor.last_tweet_id))
 
-
 # function: output to rewards csv
 def output_rewards_data():
     # EXPORT OUTPUT: export to csv file to use in reply_mentions_tracker
-    field_names = ['username','user id', 'like+retweet rewards']
-    with open('like_retweet_rewards.csv', 'w') as f:
-        writer = csv.DictWriter(f, fieldnames=field_names)
+    lrr_field_names = ['UserID','Username', 'Like+Retweet Rewards']
+    with open('like_retweet_rewards.csv', 'w') as lrr:
+        writer = csv.DictWriter(lrr, fieldnames=lrr_field_names)
+        writer.writeheader()
         for user in sm_rewards.keys():
-            f.write("%s,%s,%s\n" % (username_userID[user], user, sm_rewards[user]))
-        f.write("\n")
-        f.write('Last Update: '+ str(datetime.now()))
-
+            lrr.write("%s,%s,%s\n" % (username_userID[user], user, sm_rewards[user]))
+        lrr.write("\n")
+        lrr.write('Last Update: '+ str(datetime.now()))
+    lrr.close()
 
 # function: output to tweet csvs
 def output_tweet_data(_monitor, up_to_date):
@@ -148,38 +148,36 @@ def output_tweet_data(_monitor, up_to_date):
             t.write('\n')
         t.write('Last Update: '+ str(datetime.now()))
     # USE THIS CSV AS A REFERENCE WHEN RECEIVING MOST RECENT TWEETID
-    with open('client_tweet_data.csv', 'w') as ctd:
+    with open('tweet_monitor_data.csv', 'w') as ctd:
         ctdWriter = csv.writer(ctd)
         ctd.write("%s: %s\n" % ("Last Check Complete", _monitor.completed_check))
         ctd.write("%s: %s\n" % ("Last tweet ID", _monitor.last_tweet_id))
+        ctd.write("%s: %s\n" % ("Last Updated Tweet ID for Reply_Mentions", _monitor.twtID_for_reply_mentions))
         ctd.write("Most Recent Tweet Pull: " + str(datetime.now()))
 
 
-def up_to_date_check(_tweets, _input_list):
+def up_to_date_check(_tweets):
     print("running up to date check")
     if _tweets.data == None:
+        print("Data pulled from client had no new tweets, db_tweets_tracked is up to date")
         return True
-    if len(_input_list) > 0:
-        compare_list = []
-        client_tweet_count = 0
-        for tweet in _tweets.data:
-            compare_list.append(tweet.id)
-            client_tweet_count += 1
-        if client_tweet_count > len(db_tweets_tracked):
-            return False
-        print("finished adding tweets to compare list")
-        sorted_input_list = sort_List(_input_list)
-        sorted_compare_list = sort_List(compare_list)
-        print("finished sorting tweets in each list, comparing now")
-        if sorted_compare_list[0] == sorted_input_list[0]:
+    #extra handling/checking
+    if len(db_tweets_tracked) > 0:
+        db_tweets_sorted = sort_List(db_tweets_tracked)
+        
+        db_most_recent_twt = db_tweets_sorted[0]
+        client_most_recent_twt = _tweets.data[0].id
+        
+        if client_most_recent_twt == db_most_recent_twt:
+            print("db_tweets_tracked file is up to date, however, this means monitor.last_tweet_id was wrong on client call")
             return True
-            print("Up to Date")
         else:
+            print("not up to date, latest tweet in client is not most recent in db_tracked_tweets")
             return False
-            print("Not Up to Date")
     else:
+        print("not up to date, db_tweets_tracked is empty")
         return False
-        print("Not Up to Date, Input file is empty")
+        
 
 # iterate through tweet objects to collect retweeters, likers, and replies and assign user's their respective rewards
 # iterate through each tweet and request those who liked and those who tweeted, sorting those who did both into likers_retweeters
@@ -239,127 +237,88 @@ def like_retweet_check(_client, _tweets, _monitor):
             all_tweets_tracked.append(tweet_id)
     sort_List(all_tweets_tracked)
     _monitor.last_tweet_id = all_tweets_tracked[0]
-    print("like+retweet_check - last_tweet_id: " + str(monitor.last_tweet_id))
+    print("like+retweet_check - last_tweet_id: " + str(_monitor.last_tweet_id))
 
 
-## Main Bot Logic
-try:
-    client1 = tweepy.Client(bearer_token= first_bearer)
-    monitor = tweets_monitor()
+def client_script(_bearer_token):
     
-    print("main1 - client1 and monitor created. passing monitor to read data function")
+    client = tweepy.Client(bearer_token= _bearer_token)
     
-    read_tweet_data(monitor)
+    if _bearer_token == first_bearer:
+        client_version = "client1"
+        script_version = "1"
+    elif _bearer_token == second_bearer:
+        client_version = "client2"
+        script_version = "2"
+    elif _bearer_token == third_bearer:
+        client_version = "client3"
+        script_version = "3"
+    elif _bearer_token == fourth_bearer:
+        client_version = "client4"
+        script_version = "4"
+    else:
+        print("_bearer_token unrecognized")
+
     
-    print("main1 - read data. passing tweet data paramaters to client1 tweet pull request")
+    print(client_version + " - " + client_version + " activated. passing tweet data paramaters to " + client_version + " tweet pull request")
     
     # conditon: check was not completed/set to false and the starting tweet is initial tweet (starting point)
     if monitor.completed_check == False and monitor.last_tweet_id == 1459205924573396997:
-        print("main1 - " + str(monitor.completed_check) + " monitor complete check v1.1")
-        print("main1 - last_tweet_id: " + str(monitor.last_tweet_id))
-        tweets_client1 = client1.get_users_tweets(id=quai_id, exclude=['retweets', 'replies'], since_id= monitor.last_tweet_id, tweet_fields='created_at', max_results=100)
+        print(client_version + " - " + str(monitor.completed_check) + " monitor complete check v" + script_version + ".1")
+        print(client_version + " - last_tweet_id: " + str(monitor.last_tweet_id))
+        tweets_client = client.get_users_tweets(id=quai_id, exclude=['retweets', 'replies'], since_id= monitor.last_tweet_id, tweet_fields='created_at', max_results=100)
         up_to_date = False
     
     # conditon: check was not completed/set to false and the starting tweet is not initial tweet (half-completed check)
     elif monitor.completed_check == False and monitor.last_tweet_id != 1459205924573396997:
-        print("main1 - " + str(monitor.completed_check) + " monitor complete check v1.2")
-        print("main1 - last_tweet_id: " + str(monitor.last_tweet_id))
-        tweets_client1 = client1.get_users_tweets(id=quai_id, exclude=['retweets', 'replies'], until_id= monitor.last_tweet_id, tweet_fields='created_at', max_results=100)
+        print(client_version + " - " + str(monitor.completed_check) + " monitor complete check v" + script_version + ".2")
+        print(client_version + " - last_tweet_id: " + str(monitor.last_tweet_id))
+        tweets_client = client.get_users_tweets(id=quai_id, exclude=['retweets', 'replies'], until_id= monitor.last_tweet_id, tweet_fields='created_at', max_results=100)
         up_to_date = False
     
     # conditon: check was completed/set to true and the starting tweet is not initial tweet (following successful updates)
     elif monitor.completed_check and monitor.last_tweet_id != 1459205924573396997:
-        print("main1 - " + str(monitor.completed_check) + " monitor complete check v1.3")
-        print("main1 - last_tweet_id: " + str(monitor.last_tweet_id))
-        tweets_client1 = client1.get_users_tweets(id=quai_id, exclude=['retweets', 'replies'], since_id= monitor.last_tweet_id, tweet_fields='created_at', max_results=100)
-        up_to_date = up_to_date_check(tweets_client1, db_tweets_tracked)
-        print(up_to_date)
+        print(client_version + " - " + str(monitor.completed_check) + " monitor complete check v" + script_version + ".3")
+        print(client_version + " - last_tweet_id: " + str(monitor.last_tweet_id))
+        tweets_client = client.get_users_tweets(id=quai_id, exclude=['retweets', 'replies'], since_id= monitor.last_tweet_id, tweet_fields='created_at', max_results=100)
+        up_to_date = up_to_date_check(tweets_client, db_tweets_tracked)
+        print("tweets up to date: " + str(up_to_date))
 
     # conditon: check was completed/set to true but the starting tweet is initial tweet (?)
     elif monitor.completed_check and monitor.last_tweet_id == 1459205924573396997:
-        print("main1 - " + str(monitor.completed_check) + " monitor complete check v1.4")
-        print("main1 - last_tweet_id: " + str(monitor.last_tweet_id))
-        tweets_client1 = client1.get_users_tweets(id=quai_id, exclude=['retweets', 'replies'], since_id= monitor.last_tweet_id, tweet_fields='created_at', max_results=100)
+        print(client_version + " - " + str(monitor.completed_check) + " monitor complete check v" + script_version + ".4")
+        print(client_version + " - last_tweet_id: " + str(monitor.last_tweet_id))
+        tweets_client = client.get_users_tweets(id=quai_id, exclude=['retweets', 'replies'], since_id= monitor.last_tweet_id, tweet_fields='created_at', max_results=100)
         up_to_date = False
 
     else: # handler takes file data and re-writes it back to its original data state with updated "Last Updated" Fields
         up_to_date = True
     
-    print("main1 - client1 tweet pull request complete.")
+    print(client_version + " - " + client_version + " tweet pull request complete.")
     
     if up_to_date == False:
-        print("main1 - tweets tracked not up to date. starting like_retweet check")
-        like_retweet_check(client1, tweets_client1, monitor)
-        print("main1 - like+retweet tracker complete.")
+        print(client_version + " - tweets tracked not up to date. starting like_retweet check")
+        like_retweet_check(client, tweets_client, monitor)
+        print(client_version + " - like+retweet tracker complete.")
     
-    print("main1 - begin outputting data to csv files")
+    print(client_version + " - begin outputting data to csv files")
     
     output_tweet_data(monitor, up_to_date)
     output_rewards_data()
+
+    print(client_version + " - outputted tweet data and rewards to csv")
    
-    print('main1 end script - successful output on client1')
+    print(client_version + " end script - successful output.")
+    print(str(datetime.now()))
+
+
+## Main Bot Logic
+try:
+    monitor = tweets_monitor()
+    client_script(second_bearer)
 except:
     try:
-        client2 = tweepy.Client(bearer_token= second_bearer)
-
-        print("main2 - client2 activated. passing tweet data paramaters to client2 tweet pull request")
-        
-        # conditon: check was not completed/set to false and the starting tweet is initial tweet (starting point)
-        if monitor.completed_check == False and monitor.last_tweet_id == 1459205924573396997:
-            print("main2 - " + str(monitor.completed_check) + " monitor complete check v2.1")
-            print("main2 - last_tweet_id: " + str(monitor.last_tweet_id))
-            tweets_client2 = client2.get_users_tweets(id=quai_id, exclude=['retweets', 'replies'], since_id=monitor.last_tweet_id, tweet_fields='created_at', max_results=100)
-            up_to_date = False
-        
-        # conditon: check was not completed/set to false and the starting tweet is not initial tweet (half-completed check)
-        elif monitor.completed_check == False and monitor.last_tweet_id != 1459205924573396997:
-            print("main2 - " + str(monitor.completed_check) + " monitor complete check v2.2")
-            print("main2 - last_tweet_id: " + str(monitor.last_tweet_id))
-            tweets_client2 = client2.get_users_tweets(id=quai_id, exclude=['retweets', 'replies'], until_id=monitor.last_tweet_id, tweet_fields='created_at', max_results=100)
-            up_to_date = False
-
-        # conditon: check was completed/set to true and the starting tweet is not initial tweet (following successful updates)
-        elif monitor.completed_check and monitor.last_tweet_id != 1459205924573396997:
-            print("main2 - " + str(monitor.completed_check) + " monitor complete check v2.3")
-            print("main2 - last_tweet_id: " + str(monitor.last_tweet_id))
-            tweets_client2 = client2.get_users_tweets(id=quai_id, exclude=['retweets', 'replies'], since_id=monitor.last_tweet_id, tweet_fields='created_at', max_results=100)
-            up_to_date = up_to_date_check(tweets_client2, db_tweets_tracked)
-            print(up_to_date)
-        
-        # conditon: check was completed/set to true but the starting tweet is initial tweet (?)
-        elif monitor.completed_check and monitor.last_tweet_id == 1459205924573396997:
-            print("main2 - " + str(monitor.completed_check) + " monitor complete check v2.4")
-            print("main2 - last_tweet_id: " + str(monitor.last_tweet_id))
-            tweets_client2 = client2.get_users_tweets(id=quai_id, exclude=['retweets', 'replies'], since_id=monitor.last_tweet_id, tweet_fields='created_at', max_results=100)
-            up_to_date = up_to_date_check(tweets_client2, db_tweets_tracked)
-            print(up_to_date)
-        
-        else: # handler takes file data and re-writes it back to its original data state with updated "Last Updated" Fields
-            up_to_date = True
-
-        print("main2 - client2 tweet pull request complete.")
-        
-        if up_to_date == False:
-            print("main2 - tweets tracked not up to date. starting like_retweet check")
-            like_retweet_check(client2, tweets_client2, monitor)
-            print("main2 - like+retweet tracker complete.")
-        
-        print("main2 - begin outputting data to csv files")
-
-        output_rewards_data()
-        output_tweet_data(monitor, up_to_date)
-
-        print('main2 end script - successful output on client2')
+        client_script(third_bearer)
     except:
         output_tweet_data(monitor, False)
-        
-        print("EXCEPTION: end script - unsuccessful output: function error or the clients were rate limited")
-
-
-
-
-## NEW BUSINESS LOGIC FOR HANDLING GSHEET UPDATES:
-# - add gsheet api
-# - pull citizen's twitter username data from column in social media rewards gsheet, store as key-value pair (sm_rewards{})
-# - within the key-value pair, store the csv cell location of their rewards
-# - when iterating through the final dictionary, add the latest reward value to update
+        print("unsuccessful output -- threw exception")
