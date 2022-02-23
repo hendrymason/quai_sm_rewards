288469e3c5d84de9ab418350101de13b3d4e76b8
mheez <68663997+hendrymason@users.noreply.github.com>
2022-02-23 17:44:29 -0600

    Scaling compatible with periodic updates

--- reply_mentions_tracker.py
+++ reply_mentions_tracker.py
@@ -20,13 +20,20 @@ from oauth2client.service_account import ServiceAccountCredentials
 # CONST
 quai_id = 1306071657174441985
 quai_username = 'quainetwork'
+first_tweet_date = '2021-11-20T00:00:00Z'
 # quai_profile = client.get_user(username=quai_username.data
 first_bearer = "AAAAAAAAAAAAAAAAAAAAAEHkXwEAAAAAFgCxzDEOf484cKicUHiV3DO6qcU%3DodAKdbVDHoucR6dlOzPbB719XrDMisbZAWLufgqORgLCLgKUtO"
 second_bearer = "AAAAAAAAAAAAAAAAAAAAANBOYAEAAAAAlLMei9GmJpSazEtiXx6IWZXEbhs%3DYfsE6mbEa2mLrtZKJhjvjzr4gWZ466w1doYhiWGHlwbDyJzTwx"
+third_bearer = "AAAAAAAAAAAAAAAAAAAAAJlpZQEAAAAAjwv7Gg01zTG7ck30cyqxiwQcm9U%3DPNHl3UjpTpKdAjtVcrOs8tZqprtwO6TUKl61XWrXzKKVwgkMDA"
+fourth_bearer = "AAAAAAAAAAAAAAAAAAAAAJxpZQEAAAAAexanqSH%2B10AH6FDOHTOqAlqiI8g%3D9hHfSUY0qNXjDGBwlBRNqhXelDcDYpNvnW4VGtpiroOAlabxaR"
 
 # dicts to track and store everything -> {username: [likes+retweets, replies, mentions, engagement rewards], ...}
 sm_rewards = {}
 twitter_data = {}
+
+# set most recent quai twitter for scaling and preventing recounts
+most_recent_quai_twt = 0
+last_quai_twt_date = ''
 
 # create an array to remove team usernames from dict
 team_usernames = ['The_NFT_King', 'alanorwick', 'max_bibeau','0heezus0','owenrobertson48','ShreekaraS','mechanikalk', 'Juuuuuddi', 'QuaiNetwork']
@@ -42,53 +49,60 @@ def is_empty(file):
 
 
 def read_tweet_data():
+    with open('tweet_monitor_data.csv') as ctd:
+        print("reading tweet_monitor_data for last quai tweet")
+        empty_result = is_empty('tweet_monitor_data.csv')
+        if empty_result != True:
+            for line in ctd:
+                twt_data_line = line.strip().split(": ")
+                if "Last Updated Tweet ID for Reply_Mentions" in twt_data_line:
+                    most_recent_quai_twt = twt_data_line[1]
+    ctd.close()
+    
     # sorts through an input file and adds users to a dictionary with a value of an array containing the data in its row
     with open('like_retweet_rewards.csv') as lr:
+        line_counter = 0
         for line in lr:
-            user_array = line.strip().split(',')
-            if len(user_array) == 3 and user_array[1] not in team_usernames:
-                # set user id & rewards from like+retweets output
-                sm_rewards[user_array[1]] = [int(user_array[0]), int(user_array[2]), 0, 0, [], []] 
-                # ^sm_rewards[key: username] = value: [0: int(twitter user_id), 1: int(like+retweets rewards)]
+            if line_counter == 0:
+                line_counter += 1
+                continue
+            else:
+                user_array = line.strip().split(',')
+                if len(user_array) == 3 and user_array[1] not in team_usernames:
+                    # set user id & rewards from like+retweets output
+                    sm_rewards[user_array[1]] = [int(user_array[0]), int(user_array[2]), 0, 0] 
+                    #sm_rewards[user_array[1]] = [int(user_array[0]), int(user_array[2]), 0, 0, [], []] 
+                    # ^sm_rewards[key: username] = value: [0: int(twitter user_id), 1: int(like+retweets rewards)]
     lr.close()
     # end output: [0: int(user_id), 1: int(like+retweets rewards), 2: int(reply rewards), 3: int(mention rewards), 4: reply_array[], 5: mentions_array[]]
     # utilize reply/mention arrays [] as to accommodate for scale and ensure more accurate tracking overtime
-    print("------------------------------------------------")
-    print(" --- sm_rewards ---")
-    print(sm_rewards)
-    print("------------------------------------------------")
     with open('user_data_storage.csv') as ud:
         print("reading user data storage")
         empty_result = is_empty('user_data_storage.csv')
         if empty_result != True:
             print("user data storage has data")
+            line_index = 0
             for line in ud:
-                if "Last Updated" in line:
-                    break
+                if line_index == 0:
+                    line_index += 1
+                    continue
                 elif "User Data" in line:
                     user_data = line.strip().split(',')
                     current_user = user_data[1]
-                    twitter_data[current_user] = [user_data[2], user_data[3], user_data[4], user_data[5], [],[]]
-                elif "Reply" in line:
-                    reply_data = line.strip().split(': ')
-                    twitter_data[current_user][4].append(reply_data[1])
-                elif "Mention" in line:
-                    mention_data = line.strip().split(': ')
-                    twitter_data[current_user][5].append(mention_data[1])
+                    twitter_data[current_user] = [user_data[2], user_data[3], user_data[4], user_data[5]]
+                    #twitter_data[current_user] = [user_data[2], user_data[3], user_data[4], user_data[5], [],[]]
+                elif "Last Updated" in line:
+                    break
             print("successfully read user data storage")
         else:
             print("user data storage is empty")
     ud.close()
-    print("------------------------------------------------")
-    print(" --- twitter_data ---")
-    print(twitter_data)
-    print("------------------------------------------------")
-#['1201327349486084096', '66', '4', ['1490147959517843463', '1488743566914437120', '1488742095284174853', '1488347476897964033'], []]
-# iterate through users in dictionary and reward based on cross referencing their replies with quai's mentions
 
-
-def reply_mentions_check(_client):
-    first_tweet_date = '2021-11-20T00:00:00Z'
+def reply_mentions_check(_bearer_token):
+    client = tweepy.Client(bearer_token= _bearer_token)
+    if most_recent_quai_twt != 0:
+        quai_twt_pull = client.get_tweet(id=most_recent_quai_twt, tweet_fields="created_at")
+        last_quai_twt_date = quai_twt_pull.data.created_at
     for currentUser in sm_rewards:
             
             user_replies = []
@@ -97,11 +111,15 @@ def reply_mentions_check(_client):
             reply_count = 0
 
             #check if valid
-            if len(sm_rewards[currentUser]) == 6:
-        
-                #request all users individual tweets (with no retweets and replies) THEN request all users tweets with no retweets
-                indiv_tweets = _client.get_users_tweets(id=sm_rewards[currentUser][0], exclude=['retweets', 'replies'], start_time = first_tweet_date, tweet_fields=['created_at', 'in_reply_to_user_id', 'author_id', 'text'], max_results=100)
-                no_retweets = _client.get_users_tweets(id=sm_rewards[currentUser][0], exclude='retweets', start_time = first_tweet_date, tweet_fields=['created_at', 'in_reply_to_user_id', 'author_id', 'text'], max_results=100)
+            if len(sm_rewards[currentUser]) == 4:
+                if currentUser in twitter_data:
+                    #request all users individual tweets (with no retweets and replies) THEN request all users tweets with no retweets
+                    indiv_tweets = client.get_users_tweets(id=sm_rewards[currentUser][0], exclude=['retweets', 'replies'], start_time = last_quai_twt_date, tweet_fields=['created_at', 'in_reply_to_user_id', 'author_id', 'text'], max_results=100)
+                    no_retweets = client.get_users_tweets(id=sm_rewards[currentUser][0], exclude='retweets', start_time = last_quai_twt_date, tweet_fields=['created_at', 'in_reply_to_user_id', 'author_id', 'text'], max_results=100)
+                else:
+                    #request all users individual tweets (with no retweets and replies) THEN request all users tweets with no retweets
+                    indiv_tweets = client.get_users_tweets(id=sm_rewards[currentUser][0], exclude=['retweets', 'replies'], start_time = first_tweet_date, tweet_fields=['created_at', 'in_reply_to_user_id', 'author_id', 'text'], max_results=100)
+                    no_retweets = client.get_users_tweets(id=sm_rewards[currentUser][0], exclude='retweets', start_time = first_tweet_date, tweet_fields=['created_at', 'in_reply_to_user_id', 'author_id', 'text'], max_results=100)
                 
                 #sort tweets to get just the user reply tweets
                 if isinstance(no_retweets.data, Iterable):
@@ -132,10 +150,6 @@ def reply_mentions_check(_client):
                 #reward users for specific mentions
                 sm_rewards[currentUser][3] = mention_count * 5
 
-                #store user REPLY tweets array to check later, make script faster
-                sm_rewards[currentUser][4] = user_replies
-                #store user MENTION tweets array to check later, make script faster
-                sm_rewards[currentUser][5] = user_mentions
                 #twitter_data[username] = 
                 #   [0:user_id, 1:like+retweets, 2:replies, 3:mentions, 4:reply array, 5:mention array]
                 #sm_rewards[username] = [0:user_id, 1:like+retweets]  
@@ -159,8 +173,8 @@ def aggregate_total(dict_to_aggregate):
     print("aggregating data from dictionary")
     aggregate_rewards = {}
     for user in dict_to_aggregate.keys():
-        user_total = dict_to_aggregate[user][1] + dict_to_aggregate[user][2] + dict_to_aggregate[user][3]
-        aggregate_rewards[user] = user_total
+        user_total = int(dict_to_aggregate[user][1]) + int(dict_to_aggregate[user][2]) + int(dict_to_aggregate[user][3])
+        aggregate_rewards[user] = int(user_total)
     sorted_agg_rewards = sort_rewards(aggregate_rewards)
     print("aggregated and sorted data from dictionary")
     return sorted_agg_rewards
@@ -174,12 +188,6 @@ def update_all_data():
                 twitter_data[user][1] = int(twitter_data[user][1]) + int(sm_rewards[user][1])
                 twitter_data[user][2] = int(twitter_data[user][2]) + int(sm_rewards[user][2])
                 twitter_data[user][3] = int(twitter_data[user][3]) + int(sm_rewards[user][3])
-                for twtID in sm_rewards[user][4]:
-                    if twtID not in twitter_data[user][4]:
-                        twitter_data[user][4].append(twtID)
-                for twtID in sm_rewards[user][5]:
-                    if twtID not in twitter_data[user][5]:
-                        twitter_data[user][5].append(twtID)
     else:
         print("twitter_data empty, adding sm_rewards value to twitter_data")
         for user in sm_rewards.keys():
@@ -189,54 +197,69 @@ def update_all_data():
 
 
 def output_data(dict_to_output):
-    with open('total_twitter_rewards.csv', 'w') as f:
+    ttr_field_names = ['Username', 'Total Rewards']
+    with open('total_twitter_rewards.csv', 'w') as ttr:
         print("outputting to total twitter rewards")
+        udsWriter = csv.DictWriter(ttr, fieldnames=ttr_field_names)
+        udsWriter.writeheader()
         for username in dict_to_output:
-            f.write("%s: %s\n" % (username, dict_to_output[username]))
-        f.write('\n')
-        f.write('Last Updated: '+ str(datetime.now()))
-    #uds_field_names = ['Username', 'Like+Retweet Rewards','Reply Rewards','Mention Rewards','Reply Tweets Array', 'Mentions Tweets Array']
+            ttr.write("%s: %s\n" % (username, dict_to_output[username]))
+        ttr.write('\n')
+        ttr.write('Last Updated: '+ str(datetime.now()))
+    ttr.close()
+    uds_field_names = ['Username', 'Like+Retweet Rewards','Reply Rewards','Mention Rewards']
     with open('user_data_storage.csv','w') as uds:
         print("outputting to user data storage")
+        udsWriter = csv.DictWriter(uds, fieldnames=uds_field_names)
+        udsWriter.writeheader()
         for user in twitter_data.keys():
             usr_val = twitter_data[user]
             uds.write("%s,%s,%s,%s,%s,%s\n" % ("User Data", user, usr_val[0], usr_val[1], usr_val[2], usr_val[3]))
+            """
             for twtID in usr_val[4]:
-                if type(usr_val[4]) == str:
+                if type(twtID) == int:
                     uds.write("%s: %s\n" % ("User Reply Tweet", twtID))
             for twtID in usr_val[5]:
-                if type(usr_val[4]) == str:
+                if type(twtID) == int:
                     uds.write("%s: %s\n" % ("User Mention Tweet", twtID))
+            """
         uds.write("Last Updated: " + str(datetime.now()))
+    uds.close()
+
+def reply_mentions_main(_bearer_token):
+    
+    if _bearer_token == first_bearer:
+        client_version = "client1"
+    elif _bearer_token == second_bearer:
+        client_version = "client2"
+    elif _bearer_token == third_bearer:
+        client_version = "client3"
+    elif _bearer_token == fourth_bearer:
+        client_version = "client4"
+    else:
+        print("_bearer_token unrecognized")
+    
+    print(client_version + " - " + client_version + " activated. passing tweet data paramaters to " + client_version + " for reply mentions check")
+
+    reply_mentions_check(_bearer_token)
+    print("completed reply_mentions_check")
+    
+    updated_agg_rewards = update_all_data()
+    print("updated aggregate rewards")
+    
+    output_data(updated_agg_rewards)
+    print('successful output, end of script')
 
 ## FIX: MAKE THIS FILE A FUNCTION TIE INTO LIKE+RETWEET TRACKER
 ## FIX(?): ADD WAIT FUNCTION TO HANDLE RATE REQUESTS WITHIN LIKE+RETWEET TRACKER
 try:
     read_tweet_data()
-    print("1st try - read tweet data")
-    client1 = tweepy.Client(bearer_token= first_bearer)
-    print("1st try - client1 activated")
-    print("1st try - checking replies/mentions")
-    reply_mentions_check(client1)
-    print("1st try - completed reply_mentions_check")
-    updated_agg_rewards = update_all_data()
-    print("1st try- updated aggregate rewards")
-    output_data(updated_agg_rewards)
-    print('1st try - successful output, end of script')
+    print("read tweet data")
+    reply_mentions_main(second_bearer)
 except:
-    client2 = tweepy.Client(bearer_token= second_bearer)
-    print("2nd try - client2 activated")
-    print("2nd try - checking replies/mentions")
-    reply_mentions_check(client2)
-    print("2nd try - completed reply_mentions_check")
-    updated_agg_rewards = update_all_data()
-    print("2nd try - updated aggregate rewards")
-    output_data(updated_agg_rewards)
-    print('2nd try - successful output, end of script')
+    reply_mentions_main(third_bearer)
 
-# end output: sm_rewards[user] = [0: int(user_id), 1: int(like+retweets rewards), 
-# 2: int(reply rewards), 3: int(mention rewards), 4: reply_array[], 5: mentions_array[]]
+
+# end output: sm_rewards[user] = [int(user_id), int(like+retweets rewards), int(reply rewards), int(mention rewards)]
 # data file output for each user:
-#   index0:username,index1:user_id,index2:like+retweets pts,index3:replies pts,index4:mentions pts
-#   reply_array[]
-#   mentions_array[]
+#index0:username,index1:user_id,index2:like+retweets pts,index3:replies pts,index4:mentions pts
